d32d00061be4f7f26168a9783cb67dde34f1841b
kxxy:kodez <135093578+krys-tsuda@users.noreply.github.com>
2023-11-30 02:37:10 -0800

    Calling Methods
    Console program created using Visual Studio in C# calculates income after living expense. Class created calls three methods performing math operations on user input. Main program prompts user for input, passing it on to method and displaying the return to console.

---
+++ CallingMethods/CallingMethods/MathOperations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CallingMethods
+{
+    public class MathOperations // define class name
+    {
+        // Method to calculate monthly total
+        public int MonthTotal(int rent, int power, int gas, int water, int misc)
+        {
+            return rent + power + gas + water + misc;
+        }
+
+        // Method to calculate yearly total
+        public int YearTotal(int monthlTotal)
+        {
+            return monthlTotal * 12;
+        }
+
+        // Method to calculate income after expenses
+        public int AfterExpenses(int yearIncome, int yearTotal)
+        {
+            return yearIncome - yearTotal;
+        }
+    }
+}
---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Yearly Living Expenses\n");
+            Console.WriteLine("To calculate income after living expenses please provide the following information.\nIf an expense does not apply enter 0.");
+
+            // Prompt user input for following information
+            Console.WriteLine("\nMonthly rent:");   
+            int rent = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nMonthly power:");
+            int power = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nMonthly gas:");
+            int gas = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nMonthly water:");
+            int water = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nAny other monthly expenses:");
+            int misc = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nYearly income after taxes:");
+            int yearIncome = Convert.ToInt32(Console.ReadLine());
+
+            // Creates an instance of MathOperations
+            MathOperations mathOperations = new MathOperations();
+
+            // Call the methods with user inputs
+            int monthTotal = mathOperations.MonthTotal(rent, power, gas, water, misc);
+            int yearTotal = mathOperations.YearTotal(monthTotal);
+            int afterExpenses = mathOperations.AfterExpenses(yearIncome, yearTotal);
+            
+
+            //Display the results
+            Console.WriteLine($"\nMonth Total: ${monthTotal}");
+            Console.WriteLine($"Year Total: ${yearTotal}");
+            Console.WriteLine($"Income after expenses: ${afterExpenses}");
+          
+            Console.ReadLine();
+        }
+    }
+}
