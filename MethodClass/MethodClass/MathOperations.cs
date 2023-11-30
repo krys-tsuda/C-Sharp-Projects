756ff73d49b19351876d20fde8335468ee7c531f
kxxy:kodez <135093578+krys-tsuda@users.noreply.github.com>
2023-11-30 06:16:33 -0800

    Method Class
    Created console app using Visual Studio in C# demonstrating class creation and void method that takes two integers as parameters, performs math, and displays results. Main() method calls method passing in numbers and then specifying parameters by name.

---
+++ MethodClass/MethodClass/MathOperations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodClass
+{
+    // Define a class MathOperations
+    class MathOperations
+    {
+        // Define a void method that takes two integers
+        public void PerformMath(int firstNum, int secondNum)
+        {
+            // Perform math operation on first integer
+            int result = firstNum * 2;
+
+            // Display the second integer
+            Console.WriteLine($"Result of math operation on {firstNum}: {result} \nSecond integer: {secondNum}");
+        }
+    }
+}
---
+++ MethodClass/MethodClass/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MethodClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate the MathOperations class
+            MathOperations mathOps = new MathOperations();
+
+            // Calls method passing in two numbers
+            mathOps.PerformMath(50, 100);
+
+            // Calls method specifying parameters by name
+            mathOps.PerformMath(firstNum: 17, secondNum: 66);
+        }
+    }
+}
