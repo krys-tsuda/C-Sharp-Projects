0eb9c9cb9d36bb6becd4dbdb3dad28f2745a53be
kxxy:kodez <135093578+krys-tsuda@users.noreply.github.com>
2023-11-30 05:52:53 -0800

    Method
    Console app created with Visual Studio in C# demonstrates creation of class and method with optional parameter performing math operation on user input. Main() method takes user input and calls method displaying results.

---
+++ Method/Method/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiats SalesOperations class
+            SalesOperations salesOperations = new SalesOperations();
+
+            // Prompts user for input
+            Console.WriteLine("How many doughnuts would you like?");
+            int totalDoughnuts = Convert.ToInt32(Console.ReadLine());
+
+            // Optional input parameter
+            Console.WriteLine("\nWould you like to add coffee to your order? (Yes/No)");
+            string answer = Console.ReadLine();
+
+            if (answer.ToLower() == "yes")
+            {
+                // takes input for second parameter
+                Console.WriteLine("\nHow many coffees would you like to add?");
+                int totalCoffees = Convert.ToInt32(Console.ReadLine());
+
+                // calls method with optional parameter
+                int totalCost = salesOperations.CalculateCost(totalDoughnuts, totalCoffees);
+                Console.WriteLine($"\nTotal = ${totalCost}.00");
+            }
+            else 
+            {
+                // calls method without optional parameter
+                int totalCost = salesOperations.CalculateCost(totalDoughnuts);
+                Console.WriteLine($"\nTotal = ${totalCost}.00");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Method/Method/SalesOperations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Method
+{
+    // Defines a class 
+    class SalesOperations
+    {
+        // method that can take two integeres as paramaters
+        // second is optional default value 0
+        public int CalculateCost(int totalDoughnuts, int totalCoffees = 0)
+        {
+            // performs cost calculation based on user input
+            return (2 * totalDoughnuts) + (1 * totalCoffees);
+        }
+    }
+}
