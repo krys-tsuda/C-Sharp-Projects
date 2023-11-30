b34ba4a269a289455f104ccabd06acd0585a6258
kxxy:kodez <135093578+krys-tsuda@users.noreply.github.com>
2023-11-30 04:50:11 -0800

    Main Method
    Console app created using Visual Studio in C# demonstrates class creation and method overload. Main method takes an integer and returns result as an integer. Second method takes in decimals, performs a math operation, and returns as an integer. Third method takes a string and converts to integer if possible performing a math operation and returns answer as an integer. Main() method of console app instantiates an object of class created and displays results.

---
+++ MainMethod/MainMethod/MathOperations.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MainMethod
+{
+    // Defina a MathOperations class
+    class MathOperations
+    {
+        // Method for ineger input
+        public int MathOperation(int number)
+        {
+            // Addition operation
+            int sum = number + 10;
+            return sum;
+        }
+
+        // Method for decimal input
+        public int MathOperation(decimal number)
+        {
+            // Multiplication operation 
+            int product = (int)(number * 5);
+            return product;
+        }
+
+        // Method for strin input
+        public int MathOperation(string input)
+        {
+            // Try to parse string to integer
+            if (int.TryParse(input, out int parsedNumber))
+            {
+                // Subtraction operation for parsed integers
+                int difference = parsedNumber - 3;
+                return difference;
+            }
+            else
+            {
+                // Return if string cannot be parsed to an integer
+                return -1;
+            }
+        }
+    }
+}
---
+++ MainMethod/MainMethod/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate the MathOperations class
+            MathOperations mathOps = new MathOperations();
+
+            // Calls first method with integerand displays return
+            int intResult = mathOps.MathOperation(5);
+            Console.WriteLine("Result for integer operation: " + intResult);
+
+            // Calls second method with decimal and displays return
+            int decimalResult = mathOps.MathOperation(12.5m);
+            Console.WriteLine("Result for decimal operation: " + decimalResult);
+
+            // Calls third method with string and displays return
+            int stringResult = mathOps.MathOperation("15");
+            Console.WriteLine("Result for string operation: " + stringResult);
+
+            // Calls third method with string that cannot be turned into an integer and displays return
+            int stringResult2 = mathOps.MathOperation("fifteen");
+            Console.WriteLine("Result for string operation: " + stringResult2);
+
+            Console.ReadLine();
+        }
+    }
+}
