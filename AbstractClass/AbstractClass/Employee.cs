5858896b6b26180517576780abc09c431ed958ad
kxxy:kodez <135093578+krys-tsuda@users.noreply.github.com>
2023-12-02 01:21:54 -0800

    Abstract Class
    Console app created with Visual Studio in C#. Assignment demonstrates use of an abstract class and method.

---
+++ AbstractClass/AbstractClass/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractClass
+{
+    // Declaring class Employe inherits from class Person
+    public class Employee : Person
+    {
+        public int Id { get; set;
+        }
+
+        // implementing SayName() method inside of class
+        public override void SayName()
+        {
+            string fullName = firstName + " " + lastName;
+            Console.WriteLine($"Name: {fullName}");
+        }
+    }
+}
---
+++ AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiates an Employee object
+            Employee employee = new Employee
+            {
+                firstName = "Sample",
+                lastName = "Student"
+            };
+            // Call SayName method on employee object
+            employee.SayName();
+
+            Console.ReadLine();
+        }
+    }
+}
