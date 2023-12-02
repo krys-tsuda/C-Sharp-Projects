d1c78ad12101222dd3786fddad6b868ddf29b993
kxxy:kodez <135093578+krys-tsuda@users.noreply.github.com>
2023-12-02 03:38:11 -0800

    Operators Assignment
    Console app created using Visual Studio in C#. Assignment demonstrates operator overload comparing two objects to check if they are equal or not and displaying results to console.

---
+++ Operators/Operators/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Operators
+{
+    // Declaring class Employee
+    public class Employee
+    {
+        // properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Id { get; set; }
+
+        // overload operator== to compare employee Ids
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            return employee1.Id == employee2.Id;    // employee Ids do match
+        }
+        // overload operator!= to compl
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            return employee1.Id != employee2.Id;    // employee Ids do not match
+        }
+    }
+}
---
+++ Operators/Operators/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating object of Employee class
+            Employee employee1 = new Employee()
+            {
+                FirstName = "Jackie",
+                LastName = "Moon",
+                Id = 123456
+            };
+            // Creating second object of Employee class
+            Employee employee2 = new Employee()
+            {
+                FirstName = "Sarah",
+                LastName = "Wish",
+                Id = 456789
+            };
+
+            // Compare two Employee objects using overloaded operators
+            bool areEqual = employee1 == employee2;
+            bool areNotEqual = employee1 != employee2;
+
+            // Display results
+            Console.WriteLine($"Are employee1 and employee2 equal? {areEqual}");    
+            Console.WriteLine($"Are employee1 and employee2 not equal? {areNotEqual}");
+
+            Console.ReadLine();
+        }
+    }
+}
