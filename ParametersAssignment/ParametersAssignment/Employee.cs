5971acaa7735a20d7659ee1a1b55512db40a822f
kxxy:kodez <135093578+krys-tsuda@users.noreply.github.com>
2023-12-02 04:46:51 -0800

    ParametersAssignment
    Console app created using Visual Studio in C#. Assignment demonstrates use of generic datatype.

---
+++ ParametersAssignment/ParametersAssignment/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParametersAssignment
+{   
+    // Creating class Employee that takes generic parameter
+    public class Employee <T>
+    { 
+        // property that has data tpye generic list
+        public List<T> Things { get; set; }
+    }
+}
---
+++ ParametersAssignment/ParametersAssignment/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ParametersAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiates Employee object with type string
+            Employee<string> stringEmployee = new Employee<string>();
+
+            // Assign list of strings as the property value of Things
+            stringEmployee.Things = new List<string> { "badge", "laptop", "notebook" };
+
+            // Instantiate an Employee object with type int
+            Employee<int> intEmployee = new Employee<int>();
+
+            // Assign list of integers as property value of Things
+            intEmployee.Things = new List<int> { 1, 2, 3, 4, 5 };
+
+            // Loop that prints all of the Things to console
+            Console.WriteLine("Strings:");
+            foreach (var str in stringEmployee.Things)
+            {
+                Console.WriteLine(str);
+            }
+
+            Console.WriteLine("\nIntegers:");
+            foreach (var num in intEmployee.Things)
+            {
+                Console.WriteLine(num);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
