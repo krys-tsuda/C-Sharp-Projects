82c46a52f1be6ef9a0ebbc1dc842790e8ac47646
kxxy:kodez <135093578+krys-tsuda@users.noreply.github.com>
2023-12-02 14:26:30 -0800

    StructAssignment
    Console app created using Visual Studio in C#. Assignment demonstrates use of a struct. Creates a new object of struct and assigns value , printing to console.

---
+++ StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+// Define a struct named Number
+struct Number
+{
+    // Property Amount data type decimal
+    public decimal Amount { get; set; }
+}
+class Program
+{
+    static void Main()
+    {
+        // Create an object of data type Number
+        Number myNumber = new Number();
+
+        // Assign an amount to the Amount property of object
+        myNumber.Amount = 123.45m;
+
+        // Print amount to console
+        Console.WriteLine($"The amount is: {myNumber.Amount}");
+
+        Console.ReadLine();
+    }
+}
