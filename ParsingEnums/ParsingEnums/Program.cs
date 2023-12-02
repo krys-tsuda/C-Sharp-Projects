f853f52666aecc2688dd292b70acad26965660c8
kxxy:kodez <135093578+krys-tsuda@users.noreply.github.com>
2023-12-02 06:42:24 -0800

    ParsingEnums
    Console app created using Visual Studio in C#. Assignment demonstrates use of enum, assigning user input to variable of enum data type. Try/catch block is used to catch user input error or any other errors that could occur.

---
+++ ParsingEnums/ParsingEnums/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+// Define an enum for the days of the week
+public enum DaysOfWeek
+{
+    Sunday,
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday
+}
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            // Prompt the user to enter the current day of the week
+            Console.Write("Enter the current day of the week: ");
+            string userInput = Console.ReadLine();
+
+            // Convert the user input to the enum type (ignore case)
+            DaysOfWeek currentDay = (DaysOfWeek)Enum.Parse(typeof(DaysOfWeek), userInput, true);
+
+            // Assign the value to a variable of that enum data type
+            Console.WriteLine($"The current day is: {currentDay}");
+        }
+        catch (ArgumentException)
+        {
+            // Catch an ArgumentException if the user enters an invalid day
+            Console.WriteLine("Please enter an actual day of the week.");
+        }
+        catch (Exception ex)
+        {
+            // Catch any other exceptions that may occur
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+        finally
+        {
+            Console.ReadLine();
+        }
+    }
+}
+
