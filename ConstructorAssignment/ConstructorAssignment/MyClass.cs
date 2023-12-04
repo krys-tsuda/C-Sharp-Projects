949a8514920a0821d954b55aedf3290d7b98f7d2
kxxy:kodez <135093578+krys-tsuda@users.noreply.github.com>
2023-12-03 23:44:58 -0800

    Constructor Assignment
    Console app created using Visual Studio in C#. Assignment utilizes a constant variable, declares variable using "var", and chains two constructors together.

---
+++ ConstructorAssignment/ConstructorAssignment/MyClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstructorAssignment
+{
+    // Defines a class with two constructors
+    public class MyClass
+    {
+        // Default constructor
+        public MyClass(string myName)
+        {
+            studentName = myName;   // parameter takes user input
+            programName = "TechAcademy Program";    // default parameter
+        }
+        // Constructor with parameter
+        public MyClass(string myName, string myProgram)
+        {
+            studentName = myName;   // parameter takes user input
+            programName = myProgram;    // parameter takes user input
+        }
+        // class properties
+        public string studentName { get; set; }
+        public string programName { get; set; }
+    }
+}
---
+++ ConstructorAssignment/ConstructorAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConstructorAssignment
+{
+    class Program
+    {
+        // Creating a constant variable
+        const string programType = "Assignment";
+        static void Main(string[] args)
+        {
+            // Variable using keyword "var"
+            var assignmentName = "Constructor";
+            Console.WriteLine($"{assignmentName} {programType}");
+
+            Console.WriteLine("\nWhat's your name?");
+            string myName = Console.ReadLine();
+            Console.WriteLine("\nWhat program are you in?");
+            string myProgram = Console.ReadLine();
+
+            // Chaining constructors together
+            MyClass studentAssignment = new MyClass(myName);
+            MyClass studentAssignment2 = new MyClass(myName, myProgram);
+
+            // Display output to console
+            Console.WriteLine($"\nStudent Name: {studentAssignment.studentName} \nProgram Name: {studentAssignment.programName}");
+            Console.WriteLine($"\nStudent Name: {studentAssignment2.studentName} \nProgram Name: {studentAssignment2.programName}");
+
+            Console.ReadLine();
+        }
+    }
+}
