065433ad1e832a424c7685444cff92d0be1512c0
kxxy:kodez <135093578+krys-tsuda@users.noreply.github.com>
2023-12-03 19:00:39 -0800

    Input Assignment
    Console app created using Visual Studio in C#. Assignment demonstrates use of System.IO and logs user input to .txt file and then prints .txt file back to console.

---
+++ Input Assignment/Input Assignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace Input_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number.");    // prompts user for number
+            int number = Convert.ToInt32(Console.ReadLine());   // converts input to int
+            string text = number.ToString();    // converts text to be logged to string
+            File.WriteAllText(@"F:\TechAcademy\WorkingBranch\Logs\inputlog.txt", text);    // writing text to given file path
+            Console.WriteLine   // prints text file to console
+            (
+                "\nText from inputlog.txt: " + File.ReadAllText(@"F:\TechAcademy\WorkingBranch\Logs\inputlog.txt")
+            );
+            Console.ReadLine();
+        }
+    }
+}
