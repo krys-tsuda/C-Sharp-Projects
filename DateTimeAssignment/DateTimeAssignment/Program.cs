1f36172c0bafc765d9694e0822413ce01a5384af
kxxy:kodez <135093578+krys-tsuda@users.noreply.github.com>
2023-12-03 20:19:22 -0800

    DateTimeAssignment
    Console app created using Visual Studio in C#. Prints current date and time to console, adds user input in hours to current date and time, and prints results to console.

---
+++ DateTimeAssignment/DateTimeAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DateTimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentDateTime = DateTime.Now;    // new DateTime object of the current date and time
+            Console.WriteLine($"Current Date and Time:\n{currentDateTime}");    // prints current date and time to console
+           
+            Console.WriteLine("\nPick a number.");  // prompts user for a number
+            int number = Convert.ToInt32(Console.ReadLine());   // converts input to integer
+            
+            DateTime futureDateTime = currentDateTime.AddHours(number);    // new DateTime abject adds user input as hours to current date and time 
+            Console.WriteLine($"\nTime after {number} hours:\n{futureDateTime}");   // prints future date time to console
+            
+            Console.ReadLine();
+        }
+    }
+}
