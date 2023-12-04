9cc787cf3163351caca687bca65625a5e0e955b8
kxxy:kodez <135093578+krys-tsuda@users.noreply.github.com>
2023-12-04 01:33:23 -0800

    TryCatchAssignment
    Console app created using Visual Studio in C#. Assignment uses a while loop to calculate birthyear based on user input. and try/catch block handles input errors.

---
+++ TryCatchAssignment/TryCatchAssignment/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace TryCatchAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool validAnswer = false;
+            while (!validAnswer)    // loops until condition is true
+            {
+                try
+                {
+                    Console.WriteLine("Hi there, how old are you?");    // prompts user for age
+                    int userAge  = int.Parse(Console.ReadLine());
+
+                    if (userAge <= 0)   // validate if age is zero or greater
+                    {
+                        throw new ArgumentException("Age must be greater than 0.");
+                    }
+
+                    int birthYear = DateTime.Now.Year - userAge;    // Calculate birth year
+                    Console.WriteLine($"\nYou were born in the year {birthYear}");    //Display year user was born
+                    validAnswer = true;    // exits loop
+                }
+                catch (FormatException)    // catches invalid input format error
+                {
+                    Console.WriteLine("Invalid input format. Please enter a valid number.");
+                }
+                catch (ArgumentException ex)   // argument exception
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (Exception)   // catches any other error
+                {
+                    Console.WriteLine("An error has occured. Good bye.");
+                }
+                finally    // runs last
+                {
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
